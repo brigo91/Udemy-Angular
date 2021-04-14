ef08fb52663fb1f61ae82a6c88d145ba55925eb2
brigo <brigovaczbalazs@hotmail.com>
2021-04-14 18:30:06 +0200

    Setup project and recap ngFor & ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name="";
+  //numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNunbers = [2, 4]
+  onlyOdd = false;
 }
