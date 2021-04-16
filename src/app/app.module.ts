bb6a7659ccee4b22f7c04285315c66b6672376a7
brigo <brigovaczbalazs@hotmail.com>
2021-04-16 14:11:48 +0200

    Build dropdown directive(class is added and removed but menu is not changes in the application)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShopingEditComponent } from './shopping-list/shoping-edit/shoping-edit.component';
+import { DropDownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ShopingEditComponent } from './shopping-list/shoping-edit/shoping-edit.
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShopingEditComponent
+    ShopingEditComponent,
+    DropDownDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, HostBinding, HostListener, OnInit } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropDownDirective {
+    @HostBinding('class.open') isOpen = false;
+
+    @HostListener('click') toggleOpen(){
+        this.isOpen = !this.isOpen;
+    }
+}
