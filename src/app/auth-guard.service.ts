1f83ee5ffb657eb91a2117feadd987d5f57df763
brigo <brigovaczbalazs@hotmail.com>
2021-04-28 23:50:57 +0200

    tabfix

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -16,16 +16,16 @@ export class AuthGuard implements CanActivate, CanActivateChild{
     canActivate(route: ActivatedRouteSnapshot,
                 state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-    return this.authService.isAuthenticated()
-        .then(
-            (authenticated: boolean) => {
-                if (authenticated){
-                    return true;
-                } else{
-                    this.router.navigate(['/']);
+        return this.authService.isAuthenticated()
+            .then(
+                (authenticated: boolean) => {
+                    if (authenticated){
+                        return true;
+                    } else{
+                        this.router.navigate(['/']);
+                    }
                 }
-            }
-        )
+            )
     }
 
     canActivateChild(route: ActivatedRouteSnapshot,
