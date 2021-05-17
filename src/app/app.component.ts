32db94937e1b481f11c668950a1f9258855dc167
brigo <brigovaczbalazs@hotmail.com>
2021-05-17 20:26:16 +0200

    Setup project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name="";
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 }
