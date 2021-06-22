499479605720efebac4c5d677689c77733601350
brigo <brigovaczbalazs@hotmail.com>
2021-06-22 23:57:30 +0200

    Update existing items

--- src/app/shopping-list/shopping.service.ts
+++ src/app/shopping-list/shopping.service.ts
@@ -31,4 +31,10 @@ export class ShoppingService{
         this.ingredients.push(...ingredients);
         this.ingredientsChanged.emit(this.ingredients.slice());
     }
+
+    updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
 }
