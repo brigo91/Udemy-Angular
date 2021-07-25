5a48473805e02efe85e7b8bd115b9fafe7daf0aa
brigo <brigovaczbalazs@hotmail.com>
2021-07-25 23:34:45 +0200

    Sign in users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { SigninComponent } from "./auth/signin/signin.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
@@ -17,7 +18,8 @@ const appRoutes: Routes = [
         {path: ':id/edit', component: RecipeEditComponent}
     ]},
     {path: 'shopping-list', component: ShoppingListComponent},
-    {path: 'signup', component: SignupComponent }
+    {path: 'signup', component: SignupComponent },
+    {path: 'signin', component: SigninComponent }
 ];
 
 @NgModule({
