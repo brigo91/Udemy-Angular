960098a1b7f437ef971a55bf7fad9b5215dbad59
brigo <brigovaczbalazs@hotmail.com>
2021-07-25 22:50:04 +0200

    Sign up users

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,10 @@
+import firebase from "firebase/";
+
 export class AuthService{
     signupUser(email: string, password: string){
-
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(
+                error => console.log(error)
+            )
     }
 }
