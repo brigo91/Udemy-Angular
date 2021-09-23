6961edfd02b5ced5ece80e8624dc705f0d3569be
brigo <brigovaczbalazs@hotmail.com>
2021-09-24 00:44:28 +0200

    Sending the token

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,18 +4,23 @@ import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 import { map } from 'rxjs/operators';
 import { RecipesComponent } from "../recipes/recipes.component";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService{
-    constructor(private http: HttpClient, private recipeService: RecipeService ){}
+    constructor(private http: HttpClient,
+        private recipeService: RecipeService,
+        private authService: AuthService ){}
 
     storeRecipes(){
-        return this.http.put('https://ng-recipe-book-3e2c0-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        const token = this.authService.getToken();
+        return this.http.put('https://ng-recipe-book-3e2c0-default-rtdb.europe-west1.firebasedatabase.app/recipes.json?auth=' + token,
         this.recipeService.getRecipes());
     }
 
     getRecipes(){
-        this.http.get('https://ng-recipe-book-3e2c0-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+        const token = this.authService.getToken();
+        this.http.get('https://ng-recipe-book-3e2c0-default-rtdb.europe-west1.firebasedatabase.app/recipes.json?auth=' + token)
         .pipe(
             map(
                 (recipes: Recipe[]) => {
