d098f677d70b038a511238f6b090ec5894b9b52b
brigo <brigovaczbalazs@hotmail.com>
2021-09-24 20:59:47 +0200

    Checking and using authentication status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+
+import { AuthService } from "../auth/auth.service";
 import { DataStorageService } from "../shared/data-storage.service";
 
 
@@ -7,7 +9,8 @@ import { DataStorageService } from "../shared/data-storage.service";
     templateUrl: './header.component.html'
 })
 export class HeaderComponent{
-    constructor(private dataStorageService: DataStorageService ) {}
+    constructor(private dataStorageService: DataStorageService,
+                private authService: AuthService ) {}
 
     onSaveData(){
         this.dataStorageService.storeRecipes()
@@ -20,5 +23,9 @@ export class HeaderComponent{
 
     onFetchData(){
         this.dataStorageService.getRecipes();
+    }
+
+    isAuthenticated(){
+        return this.authService.isAuthenticated();
     }
 }
