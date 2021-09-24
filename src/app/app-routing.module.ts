35730e6348ae719cdd2c6422c09c4e7ff87fe18e
brigo <brigovaczbalazs@hotmail.com>
2021-09-24 23:57:54 +0200

    Route protection and redirection example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth/auth-guard.service";
 import { SigninComponent } from "./auth/signin/signin.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
@@ -13,9 +14,9 @@ const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
     {path: 'recipes', component: RecipesComponent, children: [
         {path: '', component: RecipesStartComponent},
-        {path: 'new', component: RecipeEditComponent},
+        {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
         {path: ':id', component: RecipeDetailComponent},
-        {path: ':id/edit', component: RecipeEditComponent}
+        {path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]}
     ]},
     {path: 'shopping-list', component: ShoppingListComponent},
     {path: 'signup', component: SignupComponent },
